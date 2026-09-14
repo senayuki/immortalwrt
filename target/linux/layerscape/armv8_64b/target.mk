8aa6e77b8f34edf38db1a694a11d8bd68a62be4a
Yuki Sena <admin@senayuki.moe>
2026-09-14 10:09:41 +0800

    kernel: allow to override linux vermagic for imm 24.10.6
    Pin the override to the 24.10.6 kernel identifier:
    6.6.133-1-80d60e5e25c8b4f01df92769064a3bbe.
    
    Signed-off-by: Yuki Sena <admin@senayuki.moe>
    Backport-of: 5d9eda441cd2079701d3236ee82b196c33ca8f75
    Split-from: 243785d9fe36bfadc4dd0237d6eb45c4d78c51af

--- target/linux/layerscape/armv8_64b/target.mk
+++ target/linux/layerscape/armv8_64b/target.mk
@@ -7,6 +7,9 @@ BOARDNAME:=ARMv8 64-bit based boards
 KERNELNAME:=Image dtbs
 DEPENDS:=+@KERNEL_BTRFS_FS +@KERNEL_BTRFS_FS_POSIX_ACL
 
+# Use the ImmortalWrt 24.10.6 kernel package identifier for this kernel release.
+LINUX_VERMAGIC_OVERRIDE = $(if $(filter 6.6.133-1,$(LINUX_VERSION)-$(LINUX_RELEASE)),80d60e5e25c8b4f01df92769064a3bbe)
+
 define Target/Description
 	Build firmware images for NXP Layerscape ARMv8 64-bit based boards.
 endef
